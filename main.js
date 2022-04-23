11f14c07fb0cdfacf1c6328f675b3d838b980850
Nirhar <manishkausikhari@gmail.com>
2022-04-23 14:26:24 +0530

    Random number feature added

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
 console.log("Hello World\n");
+
+while(1){
+    setInterval(function(){   
+        console.log("Rolling the Dice: "+Math.floor((Math.random()*6)+1)); 
+    
+     }, 2000);
+}
